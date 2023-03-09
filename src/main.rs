6fda271006766ca197a3eef5df4fab7311d1bcc6
Kees van Voorthuizen <me@keesvv.nl>
2023-03-09 13:00:04 +0100

    feat: implement clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use lot::Quote;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum Command {
+    /// Reload the quote cache
+    Reload,
+}
+
+#[derive(Parser)]
+struct Args {
+    #[clap(subcommand)]
+    command: Option<Command>,
+}
+
 fn main() {
-    println!("{}", Quote::try_from("Lorem ipsum dolor sit amet").unwrap());
+    match Args::parse().command {
+        Some(Command::Reload) => todo!("reload"),
+        None => println!("{}", Quote::try_from("Lorem ipsum dolor sit amet").unwrap()),
+    };
 }
